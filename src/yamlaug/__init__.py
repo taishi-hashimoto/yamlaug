34888cddf312b720120c8fe0179a9c9263be81cb
Taishi Hashimoto <hashimoto.taishi@outlook.com>
2026-03-08 18:48:35 +0900

    feat: dynamic version info

--- src/yamlaug/__init__.py
+++ src/yamlaug/__init__.py
@@ -1,16 +1,21 @@
 "yamlaug - YAML augmentation tool (non-destructive YAML upgrader)"
 
+from importlib.metadata import PackageNotFoundError, version
+
 from .core import augment_text
 from .file_api import augment_file
 from .types import Report, WarningRecord
 
 
 __all__ = [
-	"__version__",
-	"augment_text",
-	"augment_file",
-	"Report",
-	"WarningRecord",
+    "__version__",
+    "augment_text",
+    "augment_file",
+    "Report",
+    "WarningRecord",
 ]
 
-__version__ = "0.0.1"
+try:
+    __version__ = version("yamlaug")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
